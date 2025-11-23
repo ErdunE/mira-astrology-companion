33733a84b7431bed231a52de67beb5a993c0cfae
Yue Wu <davie1787@outlook.com>
2025-11-22 20:31:29 -0500

    arn change to openai.gpt-oss-20b

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -178,10 +178,10 @@ module "api_lambda" {
   subnet_ids         = module.network_vpc.private_subnet_ids
   security_group_ids = [module.bedrock_vpce.security_group_id]
 
-  # Fill in the specific model later
-  # bedrock_model_arns = [
-  #   "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0"
-  # ]
+
+  bedrock_model_arns = [
+    "arn:aws:bedrock:us-east-1::foundation-model/openai.gpt-oss-20b-1:0"
+  ]
 }
 
 
